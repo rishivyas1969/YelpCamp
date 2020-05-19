230f3a95fd8c10122f707050b8fa608531dfa633
pigeon <pigeon@users.nigga.github.com>
2020-05-20 01:17:35 +0530

    modified

--- app.js
+++ app.js
@@ -18,7 +18,13 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes         = require("./routes/index") ;
 
 // mongoose connect
-mongoose.connect("mongodb://localhost/YelpCamp_v10", {useNewUrlParser: true, useUnifiedTopology: true}) ;
+
+var PORT = process.env.PORT || 3000 ;
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/YelpCamp_v10", {
+    useNewUrlParser: true,
+     useUnifiedTopology: true
+    }) ;
 app.use(bodyParser.urlencoded({extended: true})) ;
 app.set("view engine", "ejs") ;
 app.use(express.static(__dirname + "/public")) ;
@@ -51,6 +57,6 @@ app.use(indexRoutes) ;
 app.use("/campgrounds", campgroundRoutes) ;
 app.use("/campgrounds/:id/comments", commentRoutes) ;
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
     console.log("YelpCamp Server started!") ;
 }) ;
